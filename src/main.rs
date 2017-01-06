523caa309ecff499a734cf0540d0ffd95d1e9b47
Mitchell Bruckner <mbruckner807@gmail.com>
2017-01-06 14:50:59 -0600

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+extern crate sysfs_gpio;
+
+use sysfs_gpio::{Direction, Pin};
+use std::thread::sleep;
+use std::time::Duration;
+
+fn main() {
+    let pin = Pin::new(17);
+    pin.with_exported(|| {
+        pin.set_value(1).unwrap();
+        sleep(Duration::from_millis(200));
+        pin.set_value(0).unwrap();
+        sleep(Duration::from_millis(200));
+    });
+}

c055f220e5d0e1487f5bb0cb78cf093e9b3e4251
Mitchell Bruckner <mbruckner807@gmail.com>
2017-01-07 16:44:16 -0600

    away with the library

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,32 @@
-extern crate sysfs_gpio;
-
-use sysfs_gpio::{Direction, Pin};
 use std::thread::sleep;
 use std::time::Duration;
 
-fn main() {
-    let pin = Pin::new(17);
-    pin.export().unwrap();
-    pin.set_value(1).unwrap();
-    sleep(Duration::from_millis(500));
-    pin.set_value(0).unwrap();
-    pin.unexport().unwrap();
+use std::io::prelude::*;
+use std::io;
+use std::fs::File;
 
+fn export(pin: usize) -> Result<(), io::Error> {
+    let mut file = try!{File::open("/sys/class/gpio/export")};
+    try!{write!(file, "{}", pin)};
+    Ok(())
+}
+
+fn unexport(pin: usize) -> Result<(), io::Error> {
+    let mut file = try!{File::open("/sys/class/gpio/unexport")};
+    try!{write!(file, "{}", pin)};
+    Ok(())
+}
+
+fn write_val(pin: usize, value: usize) -> Result<(), io::Error> {
+    let mut file = try!{File::open(format!("/sys/class/gpio/{}/value", pin))};
+    try!{write!(file, "{}", value)};
+    Ok(())
+}
+
+fn main() {
+    export(17).unwrap();
+    write_val(17, 1).unwrap();
+    sleep(Duration::from_millis(1000));
+    write_val(17, 0).unwrap();
+    unexport(17).unwrap();
 }
